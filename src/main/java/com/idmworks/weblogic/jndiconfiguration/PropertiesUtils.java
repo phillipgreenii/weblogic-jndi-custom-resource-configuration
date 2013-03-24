7477e8f3da5270abd6500559f1f477fa82596f48
Phillip Green II <deathcheese@yahoo.com>
2013-03-23 22:34:48 -0400

    applied changes to make code jdk 1.5 compatible (fixes #1)

--- src/main/java/com/idmworks/weblogic/jndiconfiguration/PropertiesUtils.java
+++ src/main/java/com/idmworks/weblogic/jndiconfiguration/PropertiesUtils.java
@@ -1,8 +1,8 @@
 package com.idmworks.weblogic.jndiconfiguration;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -24,7 +24,7 @@ public class PropertiesUtils {
     final File file = new File(filename);
     final Properties properties = new Properties();
     try {
-      properties.load(new FileReader(file));
+      properties.load(new FileInputStream(file));
     } catch (FileNotFoundException ex) {
       throw new IllegalArgumentException("File not found: " + file, ex);
     } catch (IOException ex) {
